c82e77f346f768ac6841173b0fc017973f22a6d4
s2g090123 <s2g09123@yahoo.com.tw>
2018-11-28 22:52:58 +0800

    add commit

--- HW3/RSA.py
+++ HW3/RSA.py
@@ -2,7 +2,7 @@ from fractions import gcd
 import random
 
 
-def miller_rabin(N,validate_count):
+def miller_rabin(N,validate_count):         # watch https://www.youtube.com/watch?v=qdylJqXCDGs
     if (N<2):
         return False
     if(N==3 or N==2):
@@ -11,22 +11,22 @@ def miller_rabin(N,validate_count):
         return False
     u,x = 1,N-1
     r = x
-    while x % pow(2,u) != 0:
+    while x % pow(2,u) != 0:    # (N-1) mod (2^u) != 0 
         r = x / pow(2,u)
         u+=1
 
-    for i in range(validate_count):
-        candidate = random.randrange(2,N-1)
-        b = pow(candidate,r,N)
+    for i in range(validate_count):           # validate_count means I can't check the N is a prime ,but I can through many validations ,and validate it is probably a prime
+        candidate = random.randrange(2,N-1)   # candidate is 1 < candidate < N-1
+        b = pow(candidate,r,N)                # in the first validate if b = 1 or -1 that b is probably a prime
         if(b==1 or b==N-1):
             continue
-        for j in range(u-1):
-            b = pow(b,2,N)
+        for j in range(u-1):                  # in the next validate, I check b^2 mod N
+            b = pow(b,2,N)                    # if b is -1 that b is probably a prime,but if b is 1 that b is not a prime
             if(b==N-1):
                 break
             elif(b==1):
                 return False
-        else:
+        else:                                # if I can check whether it is probably a prime, that it is not a prime
             return False
     return True
 
@@ -43,11 +43,11 @@ def ext_GCD(a,b):
     else:
         x,y,A = ext_GCD(b,a%b)
         return y,x-(a//b)*y,A
-def CRT(D,P1,P2,Cipher):
-    Dp = D % (P1-1)
-    Dq = D % (P2-1)
-    (Xq,_,_) = ext_GCD(P2,P1)
-    (Xp,_,_) = ext_GCD(P1,P2)
+def CRT(D,P1,P2,Cipher):                    # x^y mod N can split to x^y mod p and x^y mod q  *N is p*q
+    Dp = D % (P1-1)                         # 化簡次方項 *PHI(P) = P-1 if P is a prime, ex: 15^2987 mod 37
+    Dq = D % (P2-1)                         # PHI(37) = 37-1 = 36, 15^(36*82+35) mod 37,through Fermat a^(P-1) mod P = 1, so it = 15^35 mod 37
+    (Xq,_,_) = ext_GCD(P2,P1)               # Mi is 對方 , Yi is Mi inverse , Ai自己
+    (Xp,_,_) = ext_GCD(P1,P2)               # watch https://www.youtube.com/watch?v=6ytuvahX1tQ
     return (Xq*P2*(square_mul(Cipher,bin(Dp)[2:],P1))+(Xp*P1)*(square_mul(Cipher,bin(Dq)[2:],P2))) % (P1*P2)
 def RSA_GO(P1,P2,text):
     N = P1*P2
